ffa48bb5f2573648bd8ea37f4e6838464915dd83
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-07-15 17:14:58 +0530

    Implement the payment service

---
+++ README.md
@@ -0,0 +1,9 @@
+In here we should we use stripe secret key for backend.
+
+(01.) first we should get the secret strip key.
+
+(02.) Then npm i stripe
+
+(03.) require the package
+
+(04.) then implement the logic
--- routes/pyamentRoute.js
+++ routes/pyamentRoute.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     try {
-        
+        stripe.charges.create({
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd",
+        }, (stripeErr, stripeRes) => {
+            if(stripeErr){
+                res.status(400).json({
+                    message: "Payment failed. Please try again",
+                    error: stripeErr
+                });
+            }else {
+                res.status(200).json({
+                    message: "Payment successful",
+                    payment: stripeRes
+                });
+            }
+        });
     } catch (error) {
-        res.status().json({
-            message: "Payment server error",
+        res.status(500).json({
+            message: "Payment internal server error",
             error: error.message
         });
     }
